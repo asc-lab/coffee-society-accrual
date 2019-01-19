2dc795b56dc10b102bfb62b6b9ef142c68ad460a
khelman <kamil.helman@altkom.pl>
2019-01-19 21:51:56 +0100

    Unify api URIs

--- accrual-web/src/main/kotlin/pl/altkom/coffee/accrual/web/controller/AccrualSearchController.kt
+++ accrual-web/src/main/kotlin/pl/altkom/coffee/accrual/web/controller/AccrualSearchController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/search")
+@RequestMapping("/api/accrual")
 class AccrualSearchController(private val queryGateway: QueryGateway) {
 
 }
